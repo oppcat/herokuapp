a3b0485ae3c4107e6a08facd7960d68d64789dd2
yinjiwang <yinjiwang@evergrande.com>
2017-10-13 16:23:15 +0800

    update rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-     render html: "¶•ªº–≠∞¢£™£¡hello, world!"
-  end
-
-  def goodBye
-     render html: "good bye"
-  end
 end
